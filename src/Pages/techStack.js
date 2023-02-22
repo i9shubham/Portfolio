b5853ccf61bb314faed2cab1b36e2a0b9611aaa2
Shubham Ingole <90132010+Shubham7350@users.noreply.github.com>
2023-02-22 14:15:04 +0530

    tech stack added with the names

--- src/Pages/techStack.js
+++ src/Pages/techStack.js
@@ -20,21 +20,21 @@ const TechStack = () => {
                     <h1 className="centre bold">My Tech Stack</h1>
                     <p className="centre">Technologies I've been working with recently</p>
                     <div className='stackflex stackflex1'>
-                        <img src={html} alt='' className='stack' />
-                        <img src={css} alt='' className='stack' />
-                        <img src={js} alt='' className='stack' />
+                        <span className='stackStyle'><img src={html} alt='HTML' className='stack' /> HTML</span>
+                        <span className='stackStyle'><img src={css} alt='CSS' className='stack' /> CSS</span>
+                        <span className='stackStyle'><img src={js} alt='JavaScript' className='stack' /> JavaScript</span>
 
-                        <img src={react} alt='' className='stack' />
-                        <img src={bootstrap} alt='' className='stack' />
-                        <img src={vscode} alt='' className='stack' />
+                        <span className='stackStyle'><img src={react} alt='React JS' className='stack' /> React JS </span>
+                        <span className='stackStyle'><img src={bootstrap} alt='Bootstrap' className='stack' /> Bootstrap </span>
+                        <span className='stackStyle'><img src={vscode} alt='VsCode' className='stack' /> VsCode </span>
 
-                        <img src={nodejs} alt='' className='stack' />
-                        <img src={express} alt='' className='stack' />
-                        <img src={mongodb} alt='' className='stack' />
+                        <span className='stackStyle'><img src={nodejs} alt='Node JS' className='stack' /> Node JS </span>
+                        <span className='stackStyle'><img src={express} alt='Express Js' className='stack' /> Express JS </span>
+                        <span className='stackStyle'><img src={mongodb} alt='MongoDB' className='stack' /> MongoDB </span>
 
-                        <img src={git} alt='' className='stack' />
-                        <img src={github} alt='' className='stack' />
-                        <img src={cplusplus} alt='' className='stack' />
+                        <span className='stackStyle'><img src={git} alt='Git' className='stack' /> Git </span>
+                        <span className='stackStyle'><img src={github} alt='GitHub' className='stack' /> GitHub</span>
+                        <span className='stackStyle'><img src={cplusplus} alt='C++' className='stack' /> C++ </span>
                     </div>
                 </div>
             </div>
