45aad2d4f06260b060764673b9586d0ed2b5a240
Shubhahm Ingole <ishubham158@gmail.com>
2024-07-14 03:38:35 +0530

    update: skill section

--- src/Pages/techStack.js
+++ src/Pages/techStack.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import './techStack.css';
 import Fade from 'react-reveal/Fade';
 import html from '../images/vscode-icons_file-type-html.svg';
@@ -8,11 +8,19 @@ import react from '../images/logos_react.svg';
 import mui from '../images/mui.png';
 import redux from '../images/redux.png';
 import git from '../images/logos_git-icon.svg';
-import mysql from '../images/mysql.png';
+import mysql from '../images/mysqlworkbench.svg';
+import sql from '../images/sql-database-generic.svg';
 import nodejs from '../images/nodejs_logo.svg';
 import express from '../images/expressjs-ar21.svg';
 import mongodb from '../images/mongodb.svg';
 import cplusplus from '../images/icons8-c++.svg';
+import docker from '../images/docker.svg'; //
+import java from '../images/java-original.svg';
+import nestjs from '../images/nestjs.svg';
+import nextjs from '../images/next-js.svg';
+import postgres from '../images/postgresql.svg';
+import postman from '../images/postman.svg';
+import typescript from '../images/typescript-icon.svg';
 
 const TechStack = () => {
     return (
@@ -27,11 +35,11 @@ const TechStack = () => {
                         <div className='stackflex stackflex1'>
                             <span className='stackStyle'>
                                 <img src={html} alt='HTML' className='stack' />{' '}
-                                HTML
+                                <span> HTML</span>
                             </span>
                             <span className='stackStyle'>
                                 <img src={css} alt='CSS' className='stack' />{' '}
-                                CSS
+                                <span> CSS</span>
                             </span>
                             <span className='stackStyle'>
                                 <img
@@ -39,7 +47,7 @@ const TechStack = () => {
                                     alt='JavaScript'
                                     className='stack'
                                 />{' '}
-                                JavaScript
+                                <span> JavaScript</span>
                             </span>
 
                             <span className='stackStyle'>
@@ -47,8 +55,8 @@ const TechStack = () => {
                                     src={cplusplus}
                                     alt='C++'
                                     className='stack'
-                                />{' '}
-                                C++{' '}
+                                />
+                                <span> C++ </span>{' '}
                             </span>
                             <span className='stackStyle'>
                                 <img
@@ -56,7 +64,7 @@ const TechStack = () => {
                                     alt='React JS'
                                     className='stack'
                                 />{' '}
-                                React JS{' '}
+                                <span> React Js</span> {' '}
                             </span>
                             <span className='stackStyle'>
                                 <img
@@ -64,7 +72,7 @@ const TechStack = () => {
                                     alt='material ui'
                                     className='stack'
                                 />{' '}
-                                Material UI{' '}
+                                <span> Material UI </span>{' '}
                             </span>
                             <span className='stackStyle'>
                                 <img
@@ -72,7 +80,7 @@ const TechStack = () => {
                                     alt='redux'
                                     className='stack'
                                 />{' '}
-                                Redux{' '}
+                                <span> Redux</span>{' '}
                             </span>
 
                             <span className='stackStyle'>
@@ -81,7 +89,7 @@ const TechStack = () => {
                                     alt='MySQL'
                                     className='stack'
                                 />{' '}
-                                MySQL
+                                <span> MySQL</span>
                             </span>
 
                             <span className='stackStyle'>
@@ -90,7 +98,7 @@ const TechStack = () => {
                                     alt='Node JS'
                                     className='stack'
                                 />{' '}
-                                Node JS{' '}
+                                <span> Node.js</span> {' '}
                             </span>
                             <span className='stackStyle'>
                                 <img
@@ -98,7 +106,7 @@ const TechStack = () => {
                                     alt='Express Js'
                                     className='stack'
                                 />{' '}
-                                Express JS{' '}
+                                <span> Express Js</span>{' '}
                             </span>
                             <span className='stackStyle'>
                                 <img
@@ -106,12 +114,72 @@ const TechStack = () => {
                                     alt='MongoDB'
                                     className='stack'
                                 />{' '}
-                                MongoDB{' '}
+                                <span> MongoDB</span>{' '}
                             </span>
 
                             <span className='stackStyle'>
                                 <img src={git} alt='Git' className='stack' />{' '}
-                                Git{' '}
+                                <span> Git</span>{' '}
+                            </span>
+                            <span className='stackStyle'>
+                                <img
+                                    src={docker}
+                                    alt='docker'
+                                    className='stack'
+                                />{' '}
+                                <span> Docker</span>{' '}
+                            </span>
+                            <span className='stackStyle'>
+                                <img src={java} alt='Java' className='stack' />{' '}
+                                <span> Java</span>{' '}
+                            </span>
+                            <span className='stackStyle'>
+                                <img
+                                    src={nestjs}
+                                    alt='Nest.js'
+                                    className='stack'
+                                />{' '}
+                                <span> Nest.js</span>{' '}
+                            </span>
+                            <span className='stackStyle'>
+                                <img
+                                    src={nextjs}
+                                    alt='Next Js'
+                                    className='stack'
+                                />{' '}
+                                <span> Next Js</span>{' '}
+                            </span>
+                            <span className='stackStyle'>
+                                <img
+                                    src={postgres}
+                                    alt='PostgreSQL'
+                                    className='stack'
+                                />{' '}
+                                <span> PostgreSQL</span>{' '}
+                            </span>
+                            <span className='stackStyle'>
+                                <img
+                                    src={postman}
+                                    alt='Postman'
+                                    className='stack'
+                                />{' '}
+                                <span> Postman</span>{' '}
+                            </span>
+                            <span className='stackStyle'>
+                                <img
+                                    src={typescript}
+                                    alt='TypeScript'
+                                    className='stack'
+                                />{' '}
+                                <span> TypeScript</span>{' '}
+                            </span>
+                            <span className='stackStyle'>
+                                <img
+                                    src={sql}
+                                    alt='SQL'
+                                    className='stack'
+                                />{' '}
+                                <span> SQL</span>{' '}
                             </span>
                         </div>
                     </div>
