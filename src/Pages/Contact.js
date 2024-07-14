2dbe99b8e4be8188fad2c66720c68a1d16d689a1
Shubhahm Ingole <ishubham158@gmail.com>
2024-07-14 18:36:00 +0530

    fix: css styling

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import Footer from '../Components/Footer';
 import './Contact.css';
 
@@ -6,51 +6,53 @@ const Contact = () => {
     return (
         <>
             <div id='contact'>
-                <div>
-                    <h1 className='bold centre feedback'>
-                        Questions and Feedbacks are welcome
-                    </h1>
-                    <a
-                        href='mailto:ishubham158@gmail.com'
-                        className='pc heading bold centre feedback'
+                <div className='contact-flex'>
+                    <div>
+                        <h1 className='bold centre feedback'>
+                            Questions and Feedbacks are welcome
+                        </h1>
+                        <a
+                            href='mailto:ershubhamingole@gmail.com'
+                            className='pc heading bold centre feedback'
+                        >
+                            ershubhamingole@gmail.com
+                        </a>
+                    </div>
+                    <form
+                        className='contact-form mobile'
+                        action='mailto:ershubhamingole@gmail.com'
+                        method='GET'
                     >
-                        ishubham158@gmail.com
-                    </a>
+                        <div className='form-group'>
+                            <input
+                                type='text'
+                                className='form-control'
+                                placeholder='Name'
+                                required
+                            />
+                        </div>
+                        <div className='form-group'>
+                            <input
+                                type='email'
+                                className='form-control'
+                                aria-describedby='emailHelp'
+                                placeholder='Email'
+                                required
+                            />
+                        </div>
+                        <div className='form-group'>
+                            <textarea
+                                className='form-control'
+                                rows='4'
+                                placeholder='Message'
+                                required
+                            />
+                        </div>
+                        <input type='submit' className='btn' value='Submit' />
+                    </form>
                 </div>
-                <form
-                    className='contact-form mobile'
-                    action='mailto:ishubham158@gmail.com'
-                    method='GET'
-                >
-                    <div className='form-group'>
-                        <input
-                            type='text'
-                            className='form-control'
-                            placeholder='Name'
-                            required
-                        />
-                    </div>
-                    <div className='form-group'>
-                        <input
-                            type='email'
-                            className='form-control'
-                            aria-describedby='emailHelp'
-                            placeholder='Email'
-                            required
-                        />
-                    </div>
-                    <div className='form-group'>
-                        <textarea
-                            className='form-control'
-                            rows='5'
-                            placeholder='Message'
-                            required
-                        />
-                    </div>
-                    <input type='submit' className='btn' value='Submit' />
-                </form>
+                <Footer />
             </div>
-            <Footer />
         </>
     );
 };
