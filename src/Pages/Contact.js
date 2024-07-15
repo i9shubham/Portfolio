c1a1a2088ee604222e8f4c39b6d3702f96c107aa
Shubhahm Ingole <ishubham158@gmail.com>
2024-07-15 14:26:08 +0530

    add: fade bottom effect

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,54 +1,69 @@
 import React from 'react';
 import Footer from '../Components/Footer';
 import './Contact.css';
+import { Fade } from 'react-reveal';
 
 const Contact = () => {
     return (
         <>
             <div id='contact'>
                 <div className='contact-flex'>
-                    <div>
-                        <h1 className='bold centre feedback'>
-                            Questions and Feedbacks are welcome
-                        </h1>
-                        <a
-                            href='mailto:ershubhamingole@gmail.com'
-                            className='pc heading bold centre feedback'
-                        >
-                            ershubhamingole@gmail.com
-                        </a>
-                    </div>
+                    <Fade bottom>
+                        <div>
+                            <h1 className='bold centre feedback'>
+                                Questions and Feedbacks are welcome
+                            </h1>
+                            <a
+                                href='mailto:ershubhamingole@gmail.com'
+                                className='pc heading bold centre feedback'
+                            >
+                                ershubhamingole@gmail.com
+                            </a>
+                        </div>
+                    </Fade>
                     <form
                         className='contact-form mobile'
                         action='mailto:ershubhamingole@gmail.com'
                         method='GET'
                     >
-                        <div className='form-group'>
+                        <Fade bottom>
+                            <div className='form-group'>
+                                <input
+                                    type='text'
+                                    className='form-control'
+                                    placeholder='Name'
+                                    required
+                                />
+                            </div>
+                        </Fade>
+                        <Fade bottom>
+                            <div className='form-group'>
+                                <input
+                                    type='email'
+                                    className='form-control'
+                                    aria-describedby='emailHelp'
+                                    placeholder='Email'
+                                    required
+                                />
+                            </div>
+                        </Fade>
+                        <Fade bottom>
+                            <div className='form-group'>
+                                <textarea
+                                    className='form-control'
+                                    rows='4'
+                                    placeholder='Message'
+                                    required
+                                />
+                            </div>
+                        </Fade>
+                        <Fade bottom>
                             <input
-                                type='text'
-                                className='form-control'
-                                placeholder='Name'
-                                required
+                                type='submit'
+                                className='btn'
+                                value='Submit'
                             />
-                        </div>
-                        <div className='form-group'>
-                            <input
-                                type='email'
-                                className='form-control'
-                                aria-describedby='emailHelp'
-                                placeholder='Email'
-                                required
-                            />
-                        </div>
-                        <div className='form-group'>
-                            <textarea
-                                className='form-control'
-                                rows='4'
-                                placeholder='Message'
-                                required
-                            />
-                        </div>
-                        <input type='submit' className='btn' value='Submit' />
+                        </Fade>
                     </form>
                 </div>
                 <Footer />
