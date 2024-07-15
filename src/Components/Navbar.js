fb1d47d766e26bd6ee7ec632c42862a11a39e98a
Shubhahm Ingole <ishubham158@gmail.com>
2024-07-15 14:23:37 +0530

    fix: text labels and order

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,30 +1,36 @@
-import React from "react";
-import './Navbar.css'
-import {AiFillLinkedin} from 'react-icons/ai'
-import {AiFillTwitterCircle} from 'react-icons/ai'
-import {AiFillGithub} from 'react-icons/ai'
+import React from 'react';
+import './Navbar.css';
+import { AiFillLinkedin } from 'react-icons/ai';
+import { AiFillTwitterCircle } from 'react-icons/ai';
+import { AiFillGithub } from 'react-icons/ai';
 
-
-const Navbar = ()=>{
-    return(
+const Navbar = () => {
+    return (
         <>
-            <div className="navbar">
-                <div className="logo">
+            <div className='navbar'>
+                <div className='logo'>
                     <h1 className='heading'>Shubham Ingole</h1>
                 </div>
-                <div className="links">
-                    <a href="#home">Home</a>
-                    <a href="#about-me">About</a>
-                    <a href="#techStack">Tech Stack</a>
-                    <a href="#myprojects">Projects</a>
-                    <a href="#contact">Contact</a>
-                    <a href="https://www.github.com/i9shubham"><AiFillGithub size='1.6em' /></a>
-                    <a href="https://www.twitter.com/i9shubham"><AiFillTwitterCircle  size='1.6em' /></a>
-                    <a href="https://www.linkedin.com/in/i9shubham"> <AiFillLinkedin size='1.6em' /></a>
+                <div className='links'>
+                    <a href='#home'>Home</a>
+                    <a href='#techStack'>Skills</a>
+                    <a href='#myprojects'>Projects</a>
+                    <a href='#about-me'>About</a>
+                    <a href='#contact'>Contact</a>
+                    <a href='https://www.github.com/i9shubham'>
+                        <AiFillGithub size='1.6em' />
+                    </a>
+                    <a href='https://www.twitter.com/i9shubham'>
+                        <AiFillTwitterCircle size='1.6em' />
+                    </a>
+                    <a href='https://www.linkedin.com/in/i9shubham'>
+                        {' '}
+                        <AiFillLinkedin size='1.6em' />
+                    </a>
                 </div>
             </div>
         </>
-    )
-}
+    );
+};
 
 export default Navbar;
